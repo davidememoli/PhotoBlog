816db870274aba2ce57e4c9609c73d33efd82cd0
Davide <flavidproject@gmail.com>
2023-09-29 09:08:53 +0200

    Preparazione ambiente sviluppo

---
+++ photoblog/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppButtonComponent } from './components/app-button/app-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ photoblog/src/app/components/app-button/app-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-button',
+  templateUrl: './app-button.component.html',
+  styleUrls: ['./app-button.component.scss']
+})
+export class AppButtonComponent {
+
+}
